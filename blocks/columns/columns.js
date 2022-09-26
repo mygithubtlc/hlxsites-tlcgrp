d70cdcd03466031cb8be64b75c5ca77d0472296e
rofe <github@rofe.com>
2022-09-26 16:45:30 +0200

    feat: image sets in columns

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,12 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((cell) => {
+      const imgs = cell.querySelectorAll('img');
+      if (imgs.length > 1) {
+        cell.classList.add('image-set');
+      }
+    });
+  });
 }
