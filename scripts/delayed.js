1d2eea969ec955492564eda47fa80073c206d498
rofe <github@rofe.com>
2022-09-27 15:21:35 +0200

    feat: add google tag manager

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,17 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// google tag manager
+const gtmId = 'UA-97477922-3';
+window.dataLayer = window.dataLayer || [];
+function gtag(key, value) {
+  window.dataLayer.push(key, value);
+}
+gtag('js', new Date());
+gtag('config', gtmId);
+
+const script = document.createElement('script');
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${gtmId}`;
+document.head.append(script);
