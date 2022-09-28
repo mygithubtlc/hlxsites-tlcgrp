a450c0cdb8d75b2f32e29d1150f31b8773965f7f
rofe <github@rofe.com>
2022-09-28 09:19:57 +0200

    feat: configure adobe launch

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,6 +5,19 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+function loadScript(url, type, callback) {
+  const head = document.querySelector('head');
+  let script = head.querySelector(`script[src="${url}"]`);
+  if (!script) {
+    script = document.createElement('script');
+    script.src = url;
+    if (type) script.setAttribute('type', type);
+    head.append(script);
+    script.onload = callback;
+    return script;
+  }
+  return script;
+}
 
 // google tag manager
 const gtmId = 'UA-97477922-3';
@@ -15,7 +28,11 @@ function gtag(key, value) {
 gtag('js', new Date());
 gtag('config', gtmId);
 
-const script = document.createElement('script');
-script.async = true;
-script.src = `https://www.googletagmanager.com/gtag/js?id=${gtmId}`;
-document.head.append(script);
+loadScript(`https://www.googletagmanager.com/gtag/js?id=${gtmId}`, 'async');
+
+// adobe launch
+const isProd = window.location.hostname.endsWith('tlcgroup.com');
+const launchUrl = isProd
+  ? 'https://assets.adobedtm.com/0b11157d649c/ea4457f87a55/launch-8ce7ba668a1c.min.js'
+  : 'https://assets.adobedtm.com/0b11157d649c/ea4457f87a55/launch-52fa4168ab4d-staging.min.js';
+loadScript(launchUrl);
