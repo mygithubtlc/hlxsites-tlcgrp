1514ab2d822851392f3b2032818fdb05de0f6d2a
rofe <github@rofe.com>
2022-10-03 22:26:40 +0200

    feat(slideshow): animate last part of text

--- blocks/slideshow/slideshow.js
+++ blocks/slideshow/slideshow.js
@@ -1,6 +1,6 @@
 export default async function decorate(block) {
   const slides = [...block.children];
-  slides.forEach((slide) => {
+  slides.forEach((slide, index) => {
     const overlay = document.createElement('div');
     overlay.classList.add('slideshow-overlay');
     [...slide.firstElementChild.children].forEach((child) => {
@@ -21,19 +21,43 @@ export default async function decorate(block) {
     });
     if (overlay.children) {
       slide.append(overlay);
+      // hide and reveal heading text after trigger word
+      const trigger = 'through';
+      overlay.querySelectorAll('h1,h2').forEach((heading) => {
+        const text = heading.textContent;
+        const pos = text.includes(trigger)
+          ? (text.indexOf(trigger) + trigger.length + 1)
+          : 0;
+        if (pos) {
+          const revealText = text.substring(pos + trigger.length + 1);
+          heading.innerHTML = `${text.substring(0, pos)}<br>
+            <span class="${index ? '' : 'slideshow-reveal'}">${revealText}</span>`;
+        }
+      });
     }
   });
 
   // setup for multiple slides
   if (slides.length > 1) {
+    let index = 0;
     setInterval(() => {
+      // remove animation from all spans
+      block.querySelectorAll('span').forEach((span) => {
+        span.classList.remove('slideshow-reveal');
+      });
       if (block.scrollWidth - block.scrollLeft > block.offsetWidth) {
         // next slide
+        index += 1;
         block.scrollLeft += block.offsetWidth;
       } else {
         // back to start
+        index = 0;
         block.scrollLeft = 0;
       }
+      // add animation to next span
+      block.children[index].querySelectorAll('span').forEach((span) => {
+        span.classList.add('slideshow-reveal');
+      });
     }, 7000);
     window.addEventListener('resize', () => {
       block.scrollLeft = 0;
