548c24dbb561cf86457e42a8dd70034161bcd00d
rofe <github@rofe.com>
2023-03-02 16:53:05 +0100

    chore: linting

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -25,6 +25,7 @@ window.targetGlobalSettings = {
 
 // google tag manager
 const gtmId = 'GTM-58VC5NG';
+// eslint-disable-next-line
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer',gtmId);
 const iframe = document.createElement('iframe');
 iframe.setAttribute('src', `https://www.googletagmanager.com/ns.html?id=${gtmId}`);
@@ -36,9 +37,10 @@ document.body.appendChild(iframe);
 // google analytics
 const gaId = 'G-ZE1NRYD0CR';
 loadScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, 'async', () => {
+  // eslint-disable-next-line
   window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', gaId);
 });
-  
+
 // adobe launch
 const isProd = window.location.hostname.endsWith('tlcgroup.com');
 const launchUrl = isProd
