31bcc9bb93edb7151d45bc35600aab3d115a16a7
Raphael Wegmueller <github@rofe.com>
2023-03-06 18:36:27 +0100

    chore: remove unused code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -27,12 +27,6 @@ window.targetGlobalSettings = {
 const gtmId = 'GTM-58VC5NG';
 // eslint-disable-next-line
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer',gtmId);
-// const iframe = document.createElement('iframe');
-// iframe.setAttribute('src', `https://www.googletagmanager.com/ns.html?id=${gtmId}`);
-// iframe.setAttribute('height', '0');
-// iframe.setAttribute('width', '0');
-// iframe.setAttribute('style', 'display:none;visibility:hidden');
-// document.body.appendChild(iframe);
 
 // google analytics
 const gaId = 'G-ZE1NRYD0CR';
