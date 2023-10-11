0dd7a6bc4d48c36296fdec9ccf3f5056ce702c1a
Md. Azam <azam.md@tlcgroup.com>
2023-10-11 12:19:50 +0530

    unused js file removed

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -4,19 +4,6 @@
  * https://www.hlx.live/developer/block-collection/embed
  */
 
-// const loadScript = (url, callback, type) => {
-//   const head = document.querySelector('head');
-//   const script = document.createElement('script');
-//   script.src = url;
-//   if (type) {
-//     script.setAttribute('type', type);
-//   }
-
-//   script.onload = callback;
-//   head.append(script);
-//   return script;
-// };
-
 const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
     <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
       scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
@@ -37,24 +24,6 @@ const embedYoutube = (url, autoplay) => {
   return embedHTML;
 };
 
-// const embedVimeo = (url, autoplay) => {
-//   const [, video] = url.pathname.split('/');
-//   const suffix = autoplay ? '?muted=1&autoplay=1' : '';
-//   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-//       <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
-//       style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
-//       frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
-//       title="Content from Vimeo" loading="lazy"></iframe>
-//     </div>`;
-//   return embedHTML;
-// };
-
-// const embedTwitter = (url) => {
-//   const embedHTML = `<blockquote class="twitter-tweet"><a href="${url.href}"></a></blockquote>`;
-//   loadScript('https://platform.twitter.com/widgets.js');
-//   return embedHTML;
-// };
-
 const loadEmbed = (block, link, autoplay) => {
   if (block.classList.contains('embed-is-loaded')) {
     return;
@@ -65,14 +34,6 @@ const loadEmbed = (block, link, autoplay) => {
       match: ['youtube', 'youtu.be'],
       embed: embedYoutube,
     },
-    // {
-    //   match: ['vimeo'],
-    //   embed: embedVimeo,
-    // },
-    // {
-    //   match: ['twitter'],
-    //   embed: embedTwitter,
-    // },
   ];
 
   const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
@@ -93,18 +54,7 @@ export default function decorate(block) {
   block.textContent = '';
 
   if (placeholder) {
-    // const wrapper = document.createElement('div');
-    // wrapper.className = 'embed-placeholder';
-    // wrapper.innerHTML =
-    //   '<div class="embed-placeholder-play"><button title="Play"></button></div>';
-    // wrapper.prepend(placeholder);
-    // wrapper.addEventListener('click', () => {
-    //   loadEmbed(block, link, true);
-    // });
-
     loadEmbed(block, link, true);
-
-    // block.append(wrapper);
   } else {
     const observer = new IntersectionObserver((entries) => {
       if (entries.some((e) => e.isIntersecting)) {
