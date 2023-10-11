efa46a85df739f9ab2d225430c8abe3fcf317c69
Md. Azam <azam.md@tlcgroup.com>
2023-10-11 11:59:12 +0530

    fixed js issue

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -31,9 +31,7 @@ const embedYoutube = (url, autoplay) => {
     [, vid] = url.pathname.split('/');
   }
   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe src="https://www.youtube.com${
-        vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed
-      }" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+      <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
       allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
     </div>`;
   return embedHTML;
@@ -78,8 +76,7 @@ const loadEmbed = (block, link, autoplay) => {
   ];
 
   const config = EMBEDS_CONFIG.find((e) =>
-    e.match.some((match) => link.includes(match))
-  );
+    e.match.some((match) => link.includes(match)));
   const url = new URL(link);
   if (config) {
     block.innerHTML = config.embed(url, autoplay);
